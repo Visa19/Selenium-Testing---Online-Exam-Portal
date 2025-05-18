7cbe5373979eb66c93a820a54605e2de4a81c961
Visa19 <logenthiranvisagan10@gmail.com>
2025-05-18 13:00:39 +0530

    Updated File

--- src/LoginAutomationTest.java
+++ src/LoginAutomationTest.java
@@ -1,4 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginAutomationTest {
+
+	public static void main(String[] args) {
+	
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selanium_Google Drive\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.leafground.com/link.xhtml");
+		
+		driver.findElement(By.linkText("Go to Dashboard")).click();
+		
+
+	}
 
 }
